c4d85bda0106291c17d4aceed8aa0b144734a11d
0luv69 <baniyarujal@gmail.com>
2025-04-03 09:51:28 +0545

    added account app

--- clinic/account/models.py
+++ clinic/account/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractUser, Permission, User
+from django.utils import timezone
+from django.conf import settings
+from django.core.validators import RegexValidator
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="doctor_profile")
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=16, blank=True) # validators should be a list
+    address = models.CharField(max_length=255, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+    
--- clinic/clinic_base/settings.py
+++ clinic/clinic_base/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'home',
-    'accounts',
+    'account',
 
 
     # Third party apps
