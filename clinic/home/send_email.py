a9edd68fdd4b7fd390a5ad6e41de468b29ab1f5e
0luv69 <baniyarujal@gmail.com>
2025-07-02 10:00:12 +0545

    Final Project Finished Fully done

--- clinic/home/send_email.py
+++ clinic/home/send_email.py
@@ -5,24 +5,33 @@ from django.core.mail import EmailMultiAlternatives
 from django.utils.html import format_html
 
 def send_custom_email(subject, message, recipient_list, fail_silently=False, image_path=None):
-    
-    html_content = format_html(
-        """
-        <div style="white-space: pre-wrap; word-wrap: break-word;">
-        {}
-        </div>
-        <img src="{}" alt="QR Code">
-        """,
-        message, image_path
-    )
-    email = EmailMultiAlternatives(
-        subject=subject,
-        body=message,
-        from_email=settings.EMAIL_HOST_USER,
-        to=recipient_list,
-    )
-    email.attach_alternative(html_content, "text/html")
-    email.send(fail_silently=fail_silently)
+
+    if image_path:
+        html_content = format_html(
+            """
+            <div style="white-space: pre-wrap; word-wrap: break-word;">
+            {}
+            </div>
+            <img src="{}" alt="QR Code">
+            """,
+            message, image_path
+        )
+        email = EmailMultiAlternatives(
+            subject=subject,
+            body=message,
+            from_email=settings.EMAIL_HOST_USER,
+            to=recipient_list,
+        )
+        email.attach_alternative(html_content, "text/html")
+        email.send(fail_silently=fail_silently)
+    else:
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            recipient_list,
+            fail_silently=fail_silently,
+        )
     
 
 def send_custom_email_async(subject, message, recipient_list, fail_silently=False):
